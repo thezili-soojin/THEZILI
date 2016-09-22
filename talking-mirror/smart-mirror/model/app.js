7730556d9d144d1e5ccdc2a2aa3ecafd40f57223
changwook <thezili.changwook@gmail.com>
2016-09-22 13:28:38 +0000

    tensorflow model

---
+++ talking-mirror/smart-mirror/model/app.js
@@ -0,0 +1,17 @@
+var PythonShell = require('python-shell');
+
+var options = {
+	mode: 'text',
+	pythonPath: '',
+	pythonOptions: ['-u'],
+	scriptPath: '',
+	args: ['value1', 'value2', 'value3']
+};
+
+PythonShell.run('Mirror_Mirror.py', options, function(err, results) {
+	if (err) throw err;
+
+	console.log('results : %j', results);
+	console.log('results.length : %d', results.length);
+	console.log('prediction : %s', results[results.length - 1]);
+});
